0644a19fa57e6bdc7c5516fe23bb09ce8acb2e98
ParkJiHo <devcrupy@gmail.com>
2023-01-18 16:57:35 +0900

    Time: 0 ms (100.00%), Space: 40.6 MB (81.57%) - LeetHub

--- LeetCode/Easy/0066-plus-one/0066-plus-one.java
+++ LeetCode/Easy/0066-plus-one/0066-plus-one.java
@@ -1,30 +1,18 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        Stack<Integer> stack = new Stack<>();
         
-        digits[digits.length-1]++;
-        
-        for(int i = digits.length-1; i >= 0; i--){
-            if(digits[i] > 9){
-                digits[i] = digits[i] % 10;
-                if(i > 0){
-                    digits[i-1]++;
-                    stack.add(digits[i]);
-                }else{
-                    stack.add(digits[i]);
-                    stack.add(1);
-                }
-            }else{
-                stack.add(digits[i]);
-            }
-            
+    for(int i = digits.length-1; i>=0; i--) {
+        if(digits[i] < 9) {
+            digits[i]++;
+            return digits;
         }
         
-        int[] answer = new int[stack.size()];
-        for(int j = 0; j < answer.length; j++){
-            answer[j] = stack.pop();
-        }
-        
-        return answer;
+        digits[i] = 0;
     }
+    
+    int[] plus = new int [digits.length+1];
+    plus[0] = 1;
+    
+    return plus;
+}
 }
